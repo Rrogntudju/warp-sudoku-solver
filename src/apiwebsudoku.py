05c0c2215f08049b0496be0570d03b741dcaba20
Rrogntudju <rrogntudju@example.com>
2021-08-08 21:26:15 -0400

    if __name__ == "__main__":

--- src/apiwebsudoku.py
+++ src/apiwebsudoku.py
@@ -7,12 +7,12 @@ http = urllib3.PoolManager()
 
 # Extract the evil puzzle link from the frame
 frameset_page = http.request("get", "http://www.websudoku.com/?level=4")
-soup = BeautifulSoup(frameset_page.data, "lxml")
+soup = BeautifulSoup(frameset_page.data, "html.parser")
 puzzle_link = soup("frame")[0]["src"] 
 
 # Extract the puzzle's values and add the empties
 puzzle_page = http.request("get", puzzle_link)
-soup = BeautifulSoup(puzzle_page.data, "lxml")
+soup = BeautifulSoup(puzzle_page.data, "html.parser")
 puzzle_values = {tag["id"] : tag["value"] for tag in soup("input", readonly=True)}
 ids = ("f" + str(j) + str(i) for i in range(9) for j in range(9))
 puzzle = "".join(puzzle_values[id] if id in puzzle_values else "0" for id in ids)
--- src/intense.py
+++ src/intense.py
@@ -1,6 +1,4 @@
-from multiprocessing.pool import Pool
 import urllib3
-import os, sys, getopt, time
 import json
 
 http = urllib3.PoolManager()
@@ -17,28 +15,32 @@ def solve (puzzle):
     else:
         return False
 
-nb_req = 10000
-help = 'intense.py -n <number of requests>'
-try:
-    opts, _ = getopt.getopt(sys.argv[1::],"hn:",["help","nbreq="])
-except getopt.GetoptError:
-    print(help)
-    sys.exit(2)
-for opt, arg in opts:
-    if opt in ('-h', "--help"):
+if __name__ == "__main__":
+    from multiprocessing import Pool
+    import os, sys, getopt, time
+    
+    nb_req = 10000
+    help = 'intense.py -n <number of requests>'
+    try:
+        opts, _ = getopt.getopt(sys.argv[1::],"hn:",["help","nbreq="])
+    except getopt.GetoptError:
         print(help)
-        sys.exit()
-    elif opt in ("-n", "--nbreq"):
-        try:
-            nb_req = int(arg)
-        except:
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ('-h', "--help"):
             print(help)
-            sys.exit(2) 
+            sys.exit()
+        elif opt in ("-n", "--nbreq"):
+            try:
+                nb_req = int(arg)
+            except:
+                print(help)
+                sys.exit(2) 
 
-ts = time.time()
+    ts = time.time()
 
-with Pool(os.cpu_count()) as p:
-    solved = p.map(solve, ("700000600060001070804020005000470000089000340000039000600050709010300020003000004" for _ in range(nb_req)))
+    with Pool(os.cpu_count()) as p:
+        solved = p.map(solve, ("700000600060001070804020005000470000089000340000039000600050709010300020003000004" for _ in range(nb_req)))
 
-print('{}/{} puzzles solved'.format(solved.count(True), len(solved)))
-print("{:.5f} sec.".format(round(time.time() - ts, 5)))
+    print('{}/{} puzzles solved'.format(solved.count(True), len(solved)))
+    print("{:.5f} sec.".format(round(time.time() - ts, 5)))
